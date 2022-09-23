4ca84a6e8736496d5adf13d466c67f1978552b82
Michael Nelson <revnelson@gmail.com>
2022-09-23 13:48:25 +0800

    Add pm2 scripts to package.json
    Signed-off-by: Michael Nelson <revnelson@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import houdini from "houdini/vite"
 
-// import autoImport from "sveltekit-autoimport"
-// import watchAndRun from "@kitql/vite-plugin-watch-and-run"
-
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		houdini(),
-		sveltekit(),
-		// autoImport({
-		// 	components: [{ name: "./src/elements", flat: true }],
-
-		// 	module: {
-		// 		svelte: ["onMount"],
-		// 	},
-		// }),
-	],
-	// server:
-	// 	process.env.NODE_ENV === "development"
-	// 		? {
-	// 				host: "0.0.0.0",
-	// 				port: 8141,
-	// 				strictPort: true,
-	// 				hmr: {
-	// 					clientPort: 443,
-	// 				},
-	// 				fs: {
-	// 					allow: [["."], searchForWorkspaceRoot(process.cwd()), "packages/"],
-	// 				},
-	// 		  }
-	// 		: {
-	// 				fs: {
-	// 					allow: [searchForWorkspaceRoot(process.cwd()), "packages/"],
-	// 				},
-	// 		  },
+	plugins: [houdini(), sveltekit()],
 }
 
 export default config
